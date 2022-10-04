5e63a1f24d41b2e3122e0763484a24a58a9c434f
aweidric <114464194+aweidric@users.noreply.github.com>
2022-10-04 17:10:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ function list_create(){
     ];
     var s = [
         '东风夜放花千树，更吹落、星如雨。<br>宝马雕车香满路。<br>凤箫声动，玉壶光转，一夜鱼龙舞。<br>\
-        蛾儿雪柳黄金缕，笑语盈盈暗香去。<br>众里寻他千百度，蓦然回首，那人却在，灯火阑珊处。'，
+        蛾儿雪柳黄金缕，笑语盈盈暗香去。<br>众里寻他千百度，蓦然回首，那人却在，灯火阑珊处。',
         '伫倚危楼风细细，望极春愁，黯黯生天际。<br>草色烟光残照里，无言谁会凭阑意。<br>\
         拟把疏狂图一醉，对酒当歌，强乐还无味。<br>衣带渐宽终不悔，为伊消得人憔悴。 ',
         '槛菊愁烟兰泣露。罗幕轻寒，燕子双飞去。明月不谙离恨苦。斜光到晓穿朱户。<br>\
          昨夜西风凋碧树。独上高楼，望尽天涯路。欲寄彩笺兼尺素。山长水阔知何处。'
     ];
     var srcs = [
-        'https://tse2-mm.cn.bing.net/th/id/OIP-C.jveaZZJguzXq6WSYrz5vFAHaEs?w=261&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7',
-        'https://tse2-mm.cn.bing.net/th/id/OIP-C.jveaZZJguzXq6WSYrz5vFAHaEs?w=261&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7',
-        'https://tse2-mm.cn.bing.net/th/id/OIP-C.jveaZZJguzXq6WSYrz5vFAHaEs?w=261&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7'
+        'http://m.qpic.cn/psc?/V54ZVLzA2eSbEt1IFTBl2BjjQJ0MQaZi/ruAMsa53pVQWN7FLK88i5s3fY6QsSeejnYESpQCBAEhTvGRZh0QEDTSKOSvKu2YRmTdAMfQ1SsHMQVNchGEt7IXELDaEDHo6sqHsPVqloHc!/mnull&bo=JgP7AQAAAAABB*8!&rf=photolist&t=5',
+        'http://m.qpic.cn/psc?/V54ZVLzA2eSbEt1IFTBl2BjjQJ0MQaZi/ruAMsa53pVQWN7FLK88i5s3fY6QsSeejnYESpQCBAEi8Q8yig17.bnCdhjf9ks7qqNymey7xiM3hduziC7Kmi*WTf7amrf2yxK.xbGiajK8!/mnull&bo=WAJ3AQAAAAABBww!&rf=photolist&t=5',
+        'http://m.qpic.cn/psc?/V54ZVLzA2eSbEt1IFTBl2BjjQJ0MQaZi/ruAMsa53pVQWN7FLK88i5s3fY6QsSeejnYESpQCBAEjoSBE.A88S387WPY*.UIBos2hu*hQOWnjK59Hg6zCkn.VJjSeyoRk7bSPFsK3nINo!/mnull&bo=vAKKAQAAAAABBxU!&rf=photolist&t=5'
+
     ];
     var str = '';
     for(i = 0; i<s.length; i++){
